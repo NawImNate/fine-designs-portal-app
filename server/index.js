f6f6ba25eaa57466c8dab1e7935bb95da7468b39
nawimnate <jnhatch3@gmail.com>
2023-02-21 13:58:15 -0500

    init express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-// test
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Get Success");
+  console.log(res.send);
+});
