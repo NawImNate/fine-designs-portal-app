c27cf6b0f596a2eff4598f3b141f164b8bb20a0a
Nate <jnhatch3@gmail.com>
2023-02-22 09:59:11 -0500

    imported userRoute, cors to accept 3000

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
+const cors = require("cors");
 
 app.listen(PORT, () => console.log(`Server on ${PORT}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// get route
-app.get("/", (req, res) => {
-  res.send("Get Success");
-  console.log(res.send);
-});
+// MW
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
+
+// post route
+app.use("/api/users", userRoute);
+
+// MDB connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://jnhatch3:MongoDB0894@cluster0.kfcq1mh.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() =>
+    console.log("Connected to Fine-Designs-Portal project on MongoDB ")
+  )
+  .catch((err) => console.log(err));
